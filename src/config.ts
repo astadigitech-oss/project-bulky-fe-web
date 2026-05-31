b3624cfe6939b4007212ea4f2181ae4f4909ecab
Wima Alif <37034283+gntrr@users.noreply.github.com>
2026-05-31 18:41:13 +0700

    Add env example and API base config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const apiUrl =
-  process.env.NEXT_PUBLIC_API_URL! || "http://localhost:3000";
+const backendHost = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+
+export const apiUrl = `${backendHost.replace(/\/$/, "")}/api`;
 export const cookiesKey = process.env.NEXT_PUBLIC_COOKIES_KEY! || "cookies_key";
