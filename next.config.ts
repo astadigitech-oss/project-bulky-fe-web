db2b7cf788c3b698d0b2a6f5ac13690797486508
Wima Alif <37034283+gntrr@users.noreply.github.com>
2026-06-24 10:49:39 +0700

    set wildcard subdomain

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "github.com" },
-      { protocol: "https", hostname: "api.bulky.id" },
+      { protocol: "https", hostname: "**.bulky.id" },
+      { protocol: "https", hostname: "**.astadigitalagency.com" },
       { protocol: "http", hostname: "localhost" },
       { protocol: "http", hostname: "127.0.0.1" },
     ],
